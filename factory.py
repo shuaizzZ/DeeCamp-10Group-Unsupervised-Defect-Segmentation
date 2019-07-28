c9b5a9fac9d767b2e14cb8aa4bdc200ad292aa7a
CR320 <whxscpzh@163.com>
2019-07-28 15:35:41 +0800

    'SSIM'

--- factory.py
+++ factory.py
@@ -2,10 +2,10 @@ import torch
 
 
 def load_training_model_from_factory(configs):
-    if configs['model']['name'] == 'SSIM_Net':
+    if configs['model']['name'] == 'SSIM_Net_v1':
         from model.loss import SSIM_loss
-        from model.networks import SSIM_Net
-        net = SSIM_Net(code_dim=configs['model']['code_dim'])
+        from model.networks import SSIM_Net_v1
+        net = SSIM_Net_v1(code_dim=configs['model']['code_dim'])
         loss = SSIM_loss(window_size=configs['op']['window_size'], channel=3)
         optimizer = torch.optim.Adam(net.parameters(), lr=configs['op']['learning_rate'], betas=(0.5, 0.999))
     elif configs['model']['name'] == 'SSIM_Net_v2':
