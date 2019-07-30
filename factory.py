12d8d7481a398c8b7c3dfaddbd7da436ce5fac69
CR320 <whxscpzh@163.com>
2019-07-30 23:32:50 +0800

    'RED_Net'

--- factory.py
+++ factory.py
@@ -14,6 +14,12 @@ def load_training_model_from_factory(configs):
         net = SSIM_Net_v2(code_dim=configs['model']['code_dim'])
         loss = SSIM_loss(window_size=configs['op']['window_size'], channel=3)
         optimizer = torch.optim.Adam(net.parameters(), lr=configs['op']['learning_rate'], betas=(0.5, 0.999))
+    elif configs['model']['name'] == 'RED_Net':
+        from model.loss import SSIM_loss
+        from model.networks import RED_Net
+        net = RED_Net(code_dim=configs['model']['code_dim'])
+        loss = SSIM_loss(window_size=configs['op']['window_size'], channel=3)
+        optimizer = torch.optim.Adam(net.parameters(), lr=configs['op']['learning_rate'], betas=(0.5, 0.999))
     else:
         raise Exception("Invalid model name")
 
@@ -27,6 +33,9 @@ def load_test_model_from_factory(configs):
     elif configs['model']['name'] == 'SSIM_Net_v2':
         from model.networks import SSIM_Net_v2
         net = SSIM_Net_v2(code_dim=configs['model']['code_dim'])
+    elif configs['model']['name'] == 'RED_Net':
+        from model.networks import RED_Net
+        net = RED_Net(code_dim=configs['model']['code_dim'])
     else:
         raise Exception("Invalid model name")
 
--- model/networks/__init__.py
+++ model/networks/__init__.py
@@ -1,2 +1,3 @@
 from .SSIM.ssim_v1 import SSIM_Net as SSIM_Net_v1
 from .SSIM.ssim_v2 import SSIM_Net as SSIM_Net_v2
+from .RED_Net.red_net import RED_Net
